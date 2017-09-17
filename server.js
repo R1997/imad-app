5abebf9e89e41a69e533640d338d0e93270e6f9a
R1997 <30765809+R1997@users.noreply.github.com>
2017-09-17 22:48:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get("/ui/article-one",function(req, res){
+    res.send("Article one will be served here");
+});
+
+app.get("/ui/article-two",function(req, res){
+    res.send("Article two will be served here");
+});
+
+app.get("/ui/article-three",function(req, res){
+    res.send("Article three will be served here");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
